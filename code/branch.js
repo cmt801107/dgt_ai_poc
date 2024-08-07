44411fabaaccc6512346b3aa521ff577df90549b
Joyce Cai <cmt801107>
2024-08-07 15:20:57 +0800

    update code

--- code/branch.js
+++ code/branch.js
@@ -1,32 +1,120 @@
 
 
+//form欄位異動時會更新地圖
+$("#lat").on("input", updateMap);
+$("#lng").on("input", updateMap);
+
+//filter欄位輸入時會觸發resetBranchTable
+$('#filterBranchCode').on('input', resetBranchTable);
+$('#filterBranchName').on('input', resetBranchTable);
+$('#filterBranchType').on('input', resetBranchTable);
+//filterBranchStatus 異動時會觸發resetBranchTable
+$('#filterBranchStatus').on('switchChange.bootstrapSwitch', function(event, state) {
+    resetBranchTable();
+});
+
+//點擊清除條件時會觸發resetBranchTable
+$('#clearFilter').click(function() {
+    $('#filterBranchCode').val('');
+    $('#filterBranchName').val('');
+    $('#filterBranchType').val('');
+    $('#filterBranchStatus').val('');
+    $('#filterBranchStatus').prop('checked', true).bootstrapSwitch('state', true);;
+
+    resetBranchTable();
+});
+
+function clearBranchModal() {
+    $('#branchId').val('');
+    $('#branchCode').val('');
+    $('#branchName').val('');
+    $('#branchPhone').val('');
+    $('#branchAddress').val('');
+    $('#zipCode').val('');
+    $('#lat').val('');
+    $('#lng').val('');
+    $('#branchStatus').prop('checked', true).bootstrapSwitch('state', true);;
+    $('input[name="branchType"]').prop('checked', false);
+}
+
 function openBranchModal() {
+    clearBranchModal();
     $('#branchModal').modal('show');
 }
 
-function saveBranch() {
-    //參考modal的id取得input的值    
-    var branchCode = $('#branchCode').val();
-    var branchName = $('#branchName').val();
-    var branchAddress = $('#branchAddress').val();
-    var branchPhone = $('#branchPhone').val();
-    var branchStatus = $('#branchStatus').val();
-    
-    //將取得的值組成物件
-    var branchData = {
-        branchCode: branchCode,
-        branchName: branchName,
-        branchAddress: branchAddress,
-        branchPhone: branchPhone,
-        branchStatus: branchStatus,
+function editBranchModal(id) { 
+    clearBranchModal();
+
+    //取得localstorage的資料並filter id
+    var existingData = localStorage.getItem('branchData');
+    var parsedData = existingData ? JSON.parse(existingData) : [];
+    var branchData = parsedData.find(function(branch) {
+        return branch.id == id;
+    });
+
+    //將資料填入modal
+    $("#branchId").val(branchData.id);
+    $('#branchCode').val(branchData.branchCode);
+    $('#branchName').val(branchData.branchName);
+    $('#branchPhone').val(branchData.branchPhone);
+    $('#branchAddress').val(branchData.branchAddress);
+    $('#zipCode').val(branchData.zipCode);
+    $('#lat').val(branchData.lat);
+    $('#lng').val(branchData.lng);
+    $('#branchStatus').prop('checked', branchData.branchStatus).bootstrapSwitch('state', branchData.branchStatus);;
+    $('input[name="branchType"][value="' + branchData.branchType + '"]').prop('checked', true);
+
+    $('#branchModal').modal('show');
+}   
+
+function getFormVal() {
+    //取得modal的值
+    var branchData= {
+        id: $('#branchId').val(), 
+        branchCode: $('#branchCode').val(),
+        branchName: $('#branchName').val(),
+        branchStatus: $('#branchStatus').is(':checked'),
+        branchType: $('input[name="branchType"]:checked').val(),
+        branchPhone: $('#branchPhone').val(),
+        branchAddress: $('#branchAddress').val(),
+        zipCode: $('#zipCode').val(),
+        lat: $('#lat').val(),
+        lng: $('#lng').val(),
         updateTime: new Date().toLocaleString()
     };
+    console.log(branchData);
+    return branchData;
+}
+
+function saveBranch() {
+    let branchId = $('#branchId').val();
+    let formVal = getFormVal();
 
     var existingData = localStorage.getItem('branchData');
     var parsedData = existingData ? JSON.parse(existingData) : [];
-    parsedData.push(branchData);
+
+    if (branchId) {
+        var branchData = parsedData.find(function(branch) {
+            return branch.id == branchId;
+        });
+
+        if (branchData) {
+            branchData.branchCode = formVal.branchCode;
+            branchData.branchName = formVal.branchName;
+            branchData.branchPhone = formVal.branchPhone;
+            branchData.branchAddress = formVal.branchAddress;
+            branchData.zipCode = formVal.zipCode;
+            branchData.lat = formVal.lat;
+            branchData.lng = formVal.lng;
+            branchData.branchStatus = formVal.branchStatus;
+            branchData.branchType = formVal.branchType;
+            branchData.updateTime = formVal.updateTime;
+        }
+  }else {
+        formVal.id = new Date().getTime();
+        parsedData.push(formVal);
+    }
     localStorage.setItem('branchData', JSON.stringify(parsedData));
-    console.log(parsedData);
 
     resetBranchTable();
     $('#branchModal').modal('hide');
@@ -38,14 +126,16 @@ function resetBranchTable() {
     var branchCode = $('#filterBranchCode').val();
     var branchName = $('#filterBranchName').val();
     var branchType = $('#filterBranchType').val();
-    // var branchStatus = $('#filterBranchStatus').val();
-    
+    var branchStatus = $('#filterBranchStatus').is(':checked');
+
     var existingData = localStorage.getItem('branchData');
     var parsedData = existingData ? JSON.parse(existingData) : [];
     
     var filteredData = parsedData.filter(function(branch) {
         return (branchName == "" || branch.branchName.includes(branchName)) && 
-               (branchCode == "" || branch.branchCode === branchCode);
+               (branchCode == "" || branch.branchCode === branchCode) &&
+               (branchType == "" || branch.branchType === branchType) &&
+               branch.branchStatus === branchStatus;
     });
     
     var formattedData = filteredData.map(function(branch) {
@@ -54,37 +144,52 @@ function resetBranchTable() {
             branch.branchName,
             branch.branchPhone,
             branch.branchAddress,
-            "國內分行",
-            branch.updateTime        
-        ];
-    });
+            branch.branchType,
+            branch.updateTime,
+            branch.branchStatus ? '<i class="fas fa-check"></i>' : '<i class="fas fa-times"></i>',
+            // 操作欄位
+            '<button class="btn btn-success btn-sm" onclick="editBranchModal(' + branch.id + ')"><i class="fas fa-edit"></i></button>'
+        ];  });
 
-    $('#branchTable').DataTable().clear().destroy(); // Reset the DataTable
+    // Reset the DataTable
+    $('#branchTable').DataTable().clear().destroy();
     $('#branchTable').DataTable({
         "order": [[5, "desc"]], // Sort by update time
-        "data": formattedData
+        "data": formattedData,
+        "searching": false // Disable search box
     });
+}
+
+
+//google map
+let map;
+let marker;
+
+function initMap() {
+    const initialPosition = { lat: 25.0330, lng: 121.5654 }; // 預設位置（台北101）
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 15,
+        center: initialPosition,
+    });
+    marker = new google.maps.Marker({
+        position: initialPosition,
+        map: map,
+    });
+}
+
+function updateMap() {
+    const lat = parseFloat(document.getElementById("lat").value);
+    const lng = parseFloat(document.getElementById("lng").value);
+    const position = { lat: lat, lng: lng };
+    map.setCenter(position);
+    marker.setPosition(position);
 }
 
 
 //一進入頁面會去localstorage將資料撈回來並render table
 $(document).ready(function() {
+    initMap();
     resetBranchTable();
     // 初始化 Bootstrap Switch
-    $("[name='filterBranchStatus']").bootstrapSwitch();
-});
-
-//filter欄位輸入時會觸發resetBranchTable
-$('#filterBranchCode').on('input', resetBranchTable);
-$('#filterBranchName').on('input', resetBranchTable);
-$('#filterBranchType').on('input', resetBranchTable);
-// $('#filterBranchStatus').on('input', resetBranchTable);
-
-//點擊清除條件時會觸發resetBranchTable
-$('#clearFilter').click(function() {
-    $('#filterBranchCode').val('');
-    $('#filterBranchName').val('');
-    $('#filterBranchType').val('');
-    // $('#filterBranchStatus').val('');
-    resetBranchTable();
+    $(".bootstrap-switch").bootstrapSwitch();
 });
